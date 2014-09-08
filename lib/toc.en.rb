ce1452bbbdf46045e1d869bfaf8d21046999a1bb
Michele Caramello <michele@treasure-data.com>
2014-09-07 17:28:38 -0700

    Release Note 20140902

--- lib/toc.en.rb
+++ lib/toc.en.rb
@@ -16,6 +16,7 @@ section 'overview', 'Overview' do
     article 'faq', 'Frequently Asked Questions'
   end
   category 'releasenotes', 'Release Notes' do
+    article 'releasenote-20140902', 'Release Note 20140902'
     article 'releasenote-20140826', 'Release Note 20140826'
     article 'releasenote-20140819', 'Release Note 20140819'
     article 'releasenote-20140812', 'Release Note 20140812'
