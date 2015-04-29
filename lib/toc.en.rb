9ae230ec148da0bca28029f2155245b0cd550780
Kazuki Ohta <kazuki.ohta@gmail.com>
2015-04-28 17:05:39 -0700

    fixed broken link

--- lib/toc.en.rb
+++ lib/toc.en.rb
@@ -110,7 +110,7 @@ section 'import-data', 'Data Import' do
   # end
   category 'data-connector', 'Data Connectors' do
     article 'data-connector-overview', 'Data Connector Overview'
-    article 'data-connector-s3', 'Data Connector for Amazon S3'
+    article 'data-connector-amazon-s3', 'Data Connector for Amazon S3'
   end
   category 'javascript-sdk', 'JavaScript SDK' do
     article 'javascript-sdk', 'JavaScript SDK'
